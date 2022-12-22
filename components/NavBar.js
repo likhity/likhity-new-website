8f1a27b31178e96670875f583def034ce8420986
likhity <y.vyas.likhit@gmail.com>
2022-12-22 15:36:36 -0700

    Change navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,6 +5,10 @@ import styles from "../styles/NavBar.module.scss";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
 
+import { FiGithub, FiTwitter } from "react-icons/fi";
+import { BsLinkedin } from "react-icons/bs";
+import { AiOutlineInstagram } from "react-icons/ai";
+
 import { useState } from "react";
 
 export default function NavBar({ delayedAnimation }) {
@@ -14,7 +18,7 @@ export default function NavBar({ delayedAnimation }) {
   return (
     <nav className={`${styles.navBar} ${delayedAnimation ? styles.delayedAnimation : styles.animation}`}>
       <Link href="/">
-        <a className={styles.homeLink}>L<span className={`accent fw-bold`}>V</span> <span className={styles.yLetter}>Y</span> </a>
+        <a className={styles.homeLink}>vyas<span className="accent">.</span></a>
       </Link>
       <ul className={styles.showNav}>
         <li>
@@ -40,7 +44,7 @@ export default function NavBar({ delayedAnimation }) {
         {showMobileNav ? <MdClose /> : <GiHamburgerMenu />}
       </button>
       <div className={`${styles.mobileNav} ${showMobileNav ? styles.showMobileNav : styles.hideMobileNav}`}>
-        <ul>
+        <ul className={styles.navLinks}>
           <li>
             <Link href="/journey">
               <a>Journey</a>
@@ -59,6 +63,12 @@ export default function NavBar({ delayedAnimation }) {
           <li>
             <a href="https://likhity.github.io/webdev/Likhit_Vyas_Yarramsetti_Resume_2022.pdf" className={styles.resume}>Résumé</a>
           </li>
+        </ul>
+        <ul className={styles.socialLinks}>
+          <li><a href="https://github.com/likhity"><FiGithub /> GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/likhit-vyas/"> <BsLinkedin /> LinkedIn</a></li>
+          <li><a href="https://www.instagram.com/ylikhit/"> <AiOutlineInstagram /> Instagram</a></li>
+          <li><a href="https://twitter.com/YLikhit"> <FiTwitter /> Twitter</a></li>
         </ul>
       </div>
     </nav>

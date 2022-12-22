59b29fb803801929267d7a80187c6d42a2b800d2
likhity <y.vyas.likhit@gmail.com>
2022-12-22 15:36:36 -0700

    Finished mobile nav

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,14 @@
 
 import styles from "../styles/NavBar.module.scss";
 
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+
+import { useState } from "react";
+
 export default function NavBar({ delayedAnimation }) {
+
+  const [showMobileNav, setShowMobileNav] = useState(false);
 
   return (
     <nav className={`${styles.navBar} ${delayedAnimation ? styles.delayedAnimation : styles.animation}`}>
@@ -11,6 +18,17 @@ export default function NavBar({ delayedAnimation }) {
         <li><a href="/contact">Contact</a></li>
         <li><a href="/resume" className={styles.resume}>Résumé</a></li>
       </ul>
+      <button onClick={() => setShowMobileNav(prev => !prev)} className={styles.mobileNavButton}>
+        {showMobileNav ? <MdClose /> : <GiHamburgerMenu />}
+      </button>
+      <div className={`${styles.mobileNav} ${showMobileNav ? styles.showMobileNav : styles.hideMobileNav}`}>
+        <ul>
+          <li><a href="/journey">Journey</a></li>
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/resume" className={styles.resume}>Résumé</a></li>
+        </ul>
+      </div>
     </nav>
   )
 }

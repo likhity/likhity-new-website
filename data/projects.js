681495f4a83ddb2e2894d10365ab6cb8a866cfea
likhity <y.vyas.likhit@gmail.com>
2023-02-22 21:50:07 -0700

    Add contributors to twitch chat analyzer project

--- data/projects.js
+++ data/projects.js
@@ -44,6 +44,7 @@ export default [
     link: "/projects/twitch-chat-analyzer",
     markdown: "https://raw.githubusercontent.com/likhity/twitch-stream-chat-analyzer/main/README.md",
     imageBaseUri: "https://raw.githubusercontent.com/likhity/twitch-stream-chat-analyzer/main",
+    contributors: "https://api.github.com/repos/likhity/pizza-shop/contributors",
   },
   {
     title: "Old Project Portfolio",

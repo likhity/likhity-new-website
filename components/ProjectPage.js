48e2ba8d4dd070f730dcf8f0003107171f6d39fd
likhity <y.vyas.likhit@gmail.com>
2024-03-15 20:00:21 -0700

    Update meta description

--- components/ProjectPage.js
+++ components/ProjectPage.js
@@ -45,7 +45,7 @@ export default function ProjectPage({ project }) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Hello! My name is Likhit Vyas Yarramsetti. I am currently a junior at ASU studying Computer Science and a future full-stack software engineer and filmmaker. Thank you for taking the time to check out my stuff!" />
+        <meta name="description" content="Hello! My name is Likhit Vyas Yarramsetti. I am currently a senior at ASU studying Computer Science and a future full-stack software engineer and filmmaker. Thank you for taking the time to check out my stuff!" />
         <title>{`${project.title} - Likhit Vyas Yarramsetti`}</title>
         <link rel="shortcut icon" href="/favicon.ico" />
         <style>
